27b3751a9d14c50dee7ac8ee72bb0dfaefe80a99
203 <203@academicos.cesde.edu.co>
2022-02-23 10:12:30 -0500

    se crea el controlador de llenado

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,7 +1,7 @@
 //controlando etiquetas
 
 //1. crear una variable para guardar una etiqueta
-let etiquetaTitulo = document.getElementById("titulo")
+/*let etiquetaTitulo = document.getElementById("titulo")
 console.log(etiquetaTitulo)
 
 //2. como modifcar el texto de una etiqueta
@@ -17,4 +17,27 @@ etiquetaFoto.src="img/batman2.png"
 
 //cambiar el estilo de la etiqueta
 etiquetaTitulo.classList.add("text-center")
-etiquetaTitulo.classList.add("text-danger")
+etiquetaTitulo.classList.add("text-danger")*/
+
+//TIENDA
+let productosNombre=["batimovil","camiseta batman"]
+let productosPrecio=[350000,70000]
+let productosPromocion=[true,false]
+
+console.log(productosNombre)
+console.log(productosNombre[1])
+
+//creando objetos con JS
+let usuario={
+
+    nombre:"Bruno diaz",
+    edad:48,
+    amigos:["gatubela", "mr frio", "el pinguino"]
+
+}
+
+console.log(usuario)
+console.log(usuario.amigos[1])
+
+
+
---
+++ js/llenadoTienda.js
@@ -0,0 +1,11 @@
+//creando un arreglo de objetos
+
+let productos=[
+
+
+    {nombre:"batimovil",precio:400000,descripcion:"batimovil a exala"},
+    {nombre:"camiseta batman",precio:60000,descripcion:"camiseta original"}
+
+]
+
+console.log(productos)
